3b5019720ce2964498c18e0dee900f6d57dba6d1
blurbyte <info@blurbyte.com>
2018-05-18 13:58:34 +0200

    Responsive Content and MainContent

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -1,8 +1,14 @@
 import styled from 'styled-components';
 
+import media from '../../styles/media';
+
 const MainContent = styled.article`
   grid-area: main;
   padding-bottom: 6rem;
+
+  ${media.phone`
+    padding-bottom: 2rem;
+  `};
 `;
 
 export default MainContent;
--- src/components/NextDays/Content.js
+++ src/components/NextDays/Content.js
@@ -1,5 +1,14 @@
+import styled from 'styled-components';
+
+import media from '../../styles/media';
 import BaseContent from '../Content';
 
-const Content = BaseContent.withComponent('section');
+const Section = BaseContent.withComponent('section');
+
+const Content = styled(Section)`
+  ${media.desktop`
+    padding: 0;
+  `};
+`;
 
 export default Content;
