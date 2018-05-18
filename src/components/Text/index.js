32e4e5393d13c1a71e6381696d6b5f3b7a5edc2e
blurbyte <info@blurbyte.com>
2018-05-18 10:56:14 +0200

    Updated Text

--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 
 const Text = styled.p`
   font-size: ${props => (props.big ? '1.8rem' : '1.4rem')};
-  color: #222222;
+  color: ${props => (props.dimmed ? 'rgba(34, 34, 34, 0.5)' : '#222222')};
   margin: 0;
-  text-transform: uppercase;
+  text-transform: ${props => (props.big ? 'uppercase' : 'none')};
 `;
 
 export default Text;
