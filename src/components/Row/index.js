e5ecfa8215f82715f01d66ed26576119bc6324ec
blurbyte <info@blurbyte.com>
2018-05-18 13:58:10 +0200

    Responsive Row and WeatherIcon

--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Row = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
   flex-wrap: wrap;
 `;
 
--- src/components/WeatherIcon/Wrapper.js
+++ src/components/WeatherIcon/Wrapper.js
@@ -1,8 +1,15 @@
 import styled from 'styled-components';
 
+import media from '../../styles/media';
+
 const Wrapper = styled.div`
   width: ${props => (props.small ? '5rem' : '11rem')};
   height: ${props => (props.small ? '5rem' : '11rem')};
+
+  ${media.phone`
+    width: ${props => props.small && '3.6rem'};
+    height: ${props => props.small && '3.6rem'};
+  `};
 `;
 
 export default Wrapper;
