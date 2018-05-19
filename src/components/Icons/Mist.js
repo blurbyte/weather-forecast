ed98af6b3cad1003c887793bebdbe404344c3307
blurbyte <info@blurbyte.com>
2018-05-19 19:34:34 +0200

    Added more Weather svg icons

---
+++ src/components/Icons/Mist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Mist = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 110 110">
+    <path
+      d="M90 42.5H55.449a5 5 0 1 1 0-10H90a5 5 0 1 1 0 10zm-50-5a5 5 0 0 0-5-5H20a5 5 0 1 0 0 10h15a5 5 0 0 0 5-5zM95 55a5 5 0 0 0-5-5H70.992a5 5 0 1 0 0 10H90a5 5 0 0 0 5-5zm-39.458 0a5 5 0 0 0-5-5H20a5 5 0 1 0 0 10h30.542a5 5 0 0 0 5-5zM95 72.5a5 5 0 0 0-5-5H55.449a5 5 0 1 0 0 10H90a5 5 0 0 0 5-5zm-55 0a5 5 0 0 0-5-5H20a5 5 0 1 0 0 10h15a5 5 0 0 0 5-5z"
+      fill="#fff"
+    />
+  </svg>
+);
+
+export default Mist;
