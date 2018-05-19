71c5e1f1d29178a8b53390d395d461e90fce2a7b
blurbyte <info@blurbyte.com>
2018-05-19 21:41:54 +0200

    Updated formatHour function

--- src/utils/formatHour.js
+++ src/utils/formatHour.js
@@ -1,6 +1,5 @@
 export default function formatHour(timestamp) {
   return new Date(timestamp)
     .toLocaleDateString('en-US', { hour12: false, hour: 'numeric', minute: 'numeric' })
-    .split(',')[1]
-    .trim();
+    .split(' ')[1];
 }
