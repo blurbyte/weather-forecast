e68f125c4c04f19a90232806f33f7d317ee69cd2
blurbyte <info@blurbyte.com>
2018-05-19 11:41:32 +0200

    Updated snapshots

--- src/components/App/tests/App.test.js
+++ src/components/App/tests/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+
 import App from '../index';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+jest.mock('../../Forecast', () => 'Forecast');
+
+test('renders correctly', () => {
+  const component = renderer.create(<App />);
+  expect(component.toJSON()).toMatchSnapshot();
 });
