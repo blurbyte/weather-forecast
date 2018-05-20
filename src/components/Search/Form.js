5ba987597dea73a05ff6201a715188a05d08672b
blurbyte <info@blurbyte.com>
2018-05-21 00:35:37 +0200

    Hides search on tablets and smaller devices

--- src/components/Search/Form.js
+++ src/components/Search/Form.js
@@ -1,7 +1,15 @@
+// Form hidden on tablets or smaller devices
+
 import styled from 'styled-components';
+
+import media from '../../styles/media';
 
 const Form = styled.form`
   position: relative;
+
+  ${media.tablet`
+    display: none;
+  `};
 `;
 
 export default Form;
