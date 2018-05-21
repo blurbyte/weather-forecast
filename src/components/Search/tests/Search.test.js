a2d04f55848fceb8be89d2a382ff099983df4861
blurbyte <info@blurbyte.com>
2018-05-21 22:57:45 +0200

    Updated tests and snapshots

--- src/components/Search/tests/Search.test.js
+++ src/components/Search/tests/Search.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import 'jest-styled-components';
 
 import Search from '../index';
 
@@ -11,5 +10,36 @@ jest.mock('../../Icons', () => ({
 
 test('renders correctly', () => {
   const component = renderer.create(<Search />);
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('renders correctly when some cities got found', () => {
+  const component = renderer.create(<Search />);
+
+  const instance = component.getInstance();
+  instance.setState({
+    foundCities: [
+      {
+        cityId: 234567,
+        name: 'London',
+        country: 'US'
+      },
+      {
+        cityId: 87654,
+        name: 'London',
+        country: 'GB'
+      }
+    ]
+  });
+
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('renders correctly when no matches got found', () => {
+  const component = renderer.create(<Search />);
+
+  const instance = component.getInstance();
+  instance.setState({ foundCities: [], nothingFound: true });
+
   expect(component.toJSON()).toMatchSnapshot();
 });
