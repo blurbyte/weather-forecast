8c6d5620e5ae196f6f9d6c9a5e62b78935a07f7f
blurbyte <info@blurbyte.com>
2018-05-23 19:49:08 +0200

    Services https path

--- src/services/forecast.js
+++ src/services/forecast.js
@@ -6,13 +6,13 @@ import { normalizeCurrentWeather, normalizeFiveDayForecast, normalizeCities } fr
 const API_KEY = '980482225333edda41920c0a2806ba63';
 
 export function getCurrentWeather(cityId) {
-  return fetch(`http://api.openweathermap.org/data/2.5/weather?id=${cityId}&units=metric&APPID=${API_KEY}`)
+  return fetch(`https://api.openweathermap.org/data/2.5/weather?id=${cityId}&units=metric&APPID=${API_KEY}`)
     .then(res => res.json())
     .then(data => normalizeCurrentWeather(data));
 }
 
 export function getFiveDayForecast(cityId) {
-  return fetch(`http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&units=metric&APPID=${API_KEY}`)
+  return fetch(`https://api.openweathermap.org/data/2.5/forecast?id=${cityId}&units=metric&APPID=${API_KEY}`)
     .then(res => res.json())
     .then(data => normalizeFiveDayForecast(Date.now(), data.list));
 }
@@ -20,7 +20,7 @@ export function getFiveDayForecast(cityId) {
 export function getCities(search) {
   const encodedSearch = encodeURIComponent(search);
   return fetch(
-    `http://api.openweathermap.org/data/2.5/find?q=${encodedSearch}&mode=json&cnt=8&type=like&sort=population&APPID=${API_KEY}`
+    `https://api.openweathermap.org/data/2.5/find?q=${encodedSearch}&mode=json&cnt=8&type=like&sort=population&APPID=${API_KEY}`
   )
     .then(res => res.json())
     .then(data => normalizeCities(data.list));
