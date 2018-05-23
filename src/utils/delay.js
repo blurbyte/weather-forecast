12cd54f8b2b4306ca351b886dd3d7354a5dfa691
blurbyte <info@blurbyte.com>
2018-05-23 19:57:53 +0200

    Removed unused function

--- src/utils/delay.js
+++
@@ -1,5 +0,0 @@
-// Waits for milliseconds to pass
-
-export default async function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
--- src/utils/index.js
+++ src/utils/index.js
@@ -6,4 +6,3 @@ export { default as normalizeFiveDayForecast } from './normalizeFiveDayForecast'
 export { default as removeDecimals } from './removeDecimals';
 export { default as shortDate } from './removeDecimals';
 export { default as weekday } from './weekday';
-export { default as delay } from './delay';
